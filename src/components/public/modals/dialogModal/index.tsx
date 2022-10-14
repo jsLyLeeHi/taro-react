db9d04ac4ef59e225cd3abc2a9942ac0d9b29c92
luoyong <luoyong@ggnmkj.com>
2022-10-14 16:18:22 +0800

    修改弹窗的BUG

--- src/components/public/modals/dialogModal/index.tsx
+++ src/components/public/modals/dialogModal/index.tsx
@@ -1,4 +1,5 @@
 import { Button, Popup } from "@taroify/core"
+import { ButtonColor } from "@taroify/core/button/button.shared"
 import { ButtonProps } from "@tarojs/components/types/Button"
 import React, { useEffect, useState } from 'react'
 import { View } from '@tarojs/components'
@@ -13,18 +14,23 @@ type TypeOptions = {
   title?: React.ReactNode,
   /**内容 */
   content?: React.ReactNode,
+  /**组件初始化的时候触发此函数 */
+  onInit?: (p: { onAffirm: (param?: any) => void, onCancel: () => void, setLoading: React.Dispatch<React.SetStateAction<boolean>> }) => void,
   /**拦截点击确认按钮 传入此函数将拦截确认按钮并触发此函数 */
   onPrevent?: (p: { onAffirm: (param?: any) => void, onCancel: () => void, setLoading: React.Dispatch<React.SetStateAction<boolean>>, params: any }) => void,
-  /**确认按钮文字 @default "取消" */
+  /**确认按钮文字 @default "确认" */
   confirmText?: React.ReactNode,
-  /**取消按钮文字 @default "确认" */
+  /**取消按钮文字 @default "取消" */
   cancelText?: React.ReactNode,
+  /**确认按钮style @default "" */
+  confirmCloor?: ButtonColor,
   /**是否隐藏取消按钮 @default false */
   hideCancelBtn?: boolean,
   /**openType */
-  openType?: ButtonProps.openType,
+  openType?: ButtonProps.OpenType,
   /**是否在点击遮罩层后关闭 @default true */
   closeable?: boolean
+  placement?: "top" | "right" | "bottom" | "left",
 }
 
 function Index(props: TypeOptions & { reject: () => void, resolve: (p: any) => void }) {
@@ -65,17 +71,20 @@ function Index(props: TypeOptions & { reject: () => void, resolve: (p: any) => v
   }
   useEffect(() => {
     setShow(true);
+    if (typeof props.onInit === "function") {
+      props.onInit({ onAffirm: onAffirm, onCancel, setLoading })
+    }
   }, [])
   return <View className="xm-dialog-box">
-    <Popup open={show} rounded onClose={onCancel}>
+    <Popup placement={props.placement} open={show} rounded onClose={onCancel}>
       <Popup.Backdrop closeable={props.closeable}></Popup.Backdrop>
       <View className="xm-dialog">
         {props.renderTop ? <View className="dialog-image-box">{props.renderTop}</View> : null}
-        {props.title !== null ? <View className="dialog-title s-md-2 c-gray-8">{props.title}</View> : null}
+        {props.title !== null ? <View className="dialog-title t-center s-md-2 c-gray-8">{props.title}</View> : null}
         {props.content !== null ? <View className="dialog-content s-sm c-gray-6">{props.content}</View> : null}
         <View className="dialog-actions">
           {props.hideCancelBtn === true ? null : <Button onClick={onCancel} shape="round" className="dialog-btn">{props.cancelText || "取消"}</Button>}
-          <Button shape="round" className="dialog-btn" openType={props.openType} color="primary" loading={loading}
+          <Button shape="round" className="dialog-btn" openType={props.openType} color={props.confirmCloor || "primary"} loading={loading}
             onGetUserInfo={onGetUserInfo} onGetPhoneNumber={onGetPhoneNumber} onClick={onAffirm}
           >{props.confirmText || "确定"}</Button>
         </View>
